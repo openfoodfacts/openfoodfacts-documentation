07b7d3bda9ff07c3461c0626c2a24717936f541c
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-07-07 12:32:34 +0530

    UI fix
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -10,6 +10,7 @@ import defaultMdxComponents, { createRelativeLink } from 'fumadocs-ui/mdx';
 import { openapi } from '@/lib/source';
 import { APIPage } from 'fumadocs-openapi/ui';
 import { Mermaid } from '@/components/Mermaid';
+import { getMDXComponents } from '@/mdx-components';
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -17,29 +18,15 @@ export default async function Page(props: {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
-
-  const MDXContent = page.data.body;
+  
+  const MDX = page.data.body;
 
   return (
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-        <MDXContent
-          components={{
-            ...defaultMdxComponents,
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
-            // you can add other MDX components here
-            APIPage: props => <APIPage {...openapi.getAPIPageProps(props)} />,
-              code: ({ className, children, ...props }) => {
-                if (className === 'language-mermaid') {
-                  return <Mermaid chart={String(children)} />;
-                }
-                return <code className={className} {...props}>{children}</code>;
-              },
-          }}
-        />
+        <MDX components={getMDXComponents()} />
       </DocsBody>
     </DocsPage>
   );
