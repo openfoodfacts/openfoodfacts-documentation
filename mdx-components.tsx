56005feb488e95f9bdcc9d4afc405381d84df545
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-07-09 23:25:01 +0530

    feat added accordians
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,12 +3,15 @@ import type { MDXComponents } from "mdx/types";
 import { openapi } from "@/lib/source";
 import { APIPage } from "fumadocs-openapi/ui";
 import { Mermaid } from "./components/Mermaid";
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     Mermaid,
+    Accordion,
+    Accordions,
     ...components,
   };
 }
