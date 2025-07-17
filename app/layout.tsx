a0a0b74a7ff046d93ff804575e6bd660441d7b21
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-07-17 19:48:24 +0530

    fixed the issue of favicon
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,21 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import StaticSearchDialog from "@/components/search";
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider
           theme={{
